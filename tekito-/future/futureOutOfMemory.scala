b70fcb21759f2c33f1f20c2372e9484c321b9ee0
oshiro-kazuma <ariteku@gmail.com>
2014-11-07 18:46:26 +0900

    future作り過ぎてout of memoryになるやつ

---
+++ tekito-/future/futureOutOfMemory.scala
@@ -0,0 +1,20 @@
+import java.util.Date
+import scala.collection.immutable.IndexedSeq
+import scala.concurrent._
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+import ExecutionContext.Implicits.global
+import scala.util.{Failure, Success, Random}
+
+object futureThreadLimit {
+  /** main */
+  def main(args: Array[String]) {
+    Console println "Thread作りすぎてout of memory"
+    val f: Future[IndexedSeq[Int]] = Future.sequence {
+      for (i <- 1 to 5000) yield {
+        Future{ blocking { Thread.sleep(500); println(s"end[$i] "); i} }
+      }
+    }
+    Console println Await.result(f, Duration.Inf)
+  }
+}
